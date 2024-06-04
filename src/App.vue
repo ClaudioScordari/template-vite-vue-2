65a105c0ccec34d8e2bc47f1e833ba2f8814ee56
ClaudioScordari <scordari22@gmail.com>
2024-06-04 19:30:08 +0200

    Completato template

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+        // ...
+    },
+    components: {
+        AppHeader,
+    }
+}
+</script>
+
+<template>
+    <AppHeader/>
+
+    <h1>Vue</h1>
+</template>
+
+<style lang="scss" scoped>
+@use 'assets/scss/main.scss' as *;
+</style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+        // ...
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>Header</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
